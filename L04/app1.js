e1b5d0af8eaad80c74f0bc351d9ef16528820cb6
avanishkumar5035 <146574431+avanishkumar5035@users.noreply.github.com>
2026-01-15 22:37:11 +0530

    commit

---
+++ L04/app1.js
@@ -0,0 +1,5 @@
+let math = require("./module");
+
+console.log(math.PI);
+console.log(math.add(3,5));
+console.log(math.square(3))
---
+++ L04/module.js
@@ -0,0 +1,20 @@
+let PI = 3.14;
+function add(a,b)
+{
+    return a+b;
+}
+
+let add1=(a,b)=>{
+    return a+b;
+}
+
+// let multi=(a,b)=>a+b;
+
+
+function square(a){
+    return a*a;
+}
+
+module.exports={
+    PI, add,square,
+}
