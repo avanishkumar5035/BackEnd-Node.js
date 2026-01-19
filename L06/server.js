f37e8c2f123e2764ad8148615e0daf91b4c83112
avanishkumar5035 <146574431+avanishkumar5035@users.noreply.github.com>
2026-01-19 22:47:22 +0530

    commit

---
+++ L06/server.js
@@ -0,0 +1,10 @@
+let http = require("http");
+
+let server = http.createServer((req,res)=>{
+    res.write("learing node js http to create server");
+    res.end();
+})
+
+server.listen(3000,()=>{
+    console.log("Server is running at port 3000");
+})
