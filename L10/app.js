622f0bef103e4915605c605400cda428835069a8
avanishkumar5035 <146574431+avanishkumar5035@users.noreply.github.com>
2026-01-28 20:44:08 +0530

    commit

---
+++ L10/app.js
@@ -0,0 +1,14 @@
+const { render } = require("ejs");
+let express = require("express");
+let path = require("path");
+let app = express();
+app.set("view wngine", 'ejs');
+app.set('views', path.join(__dirname, "views"));
+
+app.get("/",(req,res)=>{
+    res.render("index");
+});
+
+app.listen(4000,()=>{
+    console.log("App is running at port 4000");
+});
