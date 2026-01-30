0d1b3eee73aff00518317983867387ff23868934
avanishkumar5035 <146574431+avanishkumar5035@users.noreply.github.com>
2026-01-30 20:41:06 +0530

    commit

--- L10/app.js
+++ L10/app.js
@@ -2,11 +2,15 @@ const { render } = require("ejs");
 let express = require("express");
 let path = require("path");
 let app = express();
-app.set("view wngine", 'ejs');
+app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, "views"));
 
+let obj = {
+    id:1212
+}
+
 app.get("/log",(req,res)=>{
-    res.render("index");
+    res.render("index", {obj});
 });
 
 app.listen(4000,()=>{
