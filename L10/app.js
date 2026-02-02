34ccc1d737daba4c24c590ce37f69d528269d8ee
avanishkumar5035 <146574431+avanishkumar5035@users.noreply.github.com>
2026-02-02 20:38:43 +0530

    commit

--- L10/app.js
+++ L10/app.js
@@ -2,20 +2,40 @@ const { render } = require("ejs");
 let express = require("express");
 let path = require("path");
 let app = express();
+let bodyparser = require("body-parser");
+app.use(bodyparser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, "views"));
 
-let obj = {
-    id:1212
-}
+let todo=[
+    "reading book",
+    "Go to Market",
+    "coding",
+];
 
-app.get("/log",(req,res)=>{
-    res.render("index", {obj});
+app.get("/todo",(req,res)=>{
+    res.render("index",{todo: []});
 });
 
-app.listen(4000,()=>{
+// let obj = {
+//     id:1212
+// }
+
+// app.get("/log",(req,res)=>{
+//     res.render("index", {obj});
+// });
+
+app.get("/register",(req,res)=>{
+    res.render("index");
+});
+
+app.post("/register",(req,res)=>{
+    console.log(req.body);
+    res.send("user register successfully!.. ");
+})
+
+app.listen(3000,()=>{
     console.log("App is running at port 4000");
 });
 
 
-console.log("hello")
